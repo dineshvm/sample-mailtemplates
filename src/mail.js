ffe468f721c86d9553713dbe38b5feb8f3f499c9
Vilvanathan <dinesh.vilvanathan@siemens.com>
2020-12-14 09:43:45 +0530

    added pug compilation snippet

--- src/mail.js
+++ src/mail.js
@@ -3,8 +3,9 @@ require('dotenv').config();
 const nodemailer = require('nodemailer');
 const Email = require('email-templates');
 
+const pug = require('pug');
 
-const sendEmail = () => {
+const sendEmail1 = () => {
     let transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
@@ -24,23 +25,34 @@ const sendEmail = () => {
         { username: 'name2', taskname: 'task2', action: 'completed' }
     ]
 
+
     email.render('html', {
         taskList: tasks
     }).then((temp) => {
         console.log(temp);
-       /*  email.send({
-            message: {
-                from: 'abc@gmail.com',
-                to: 'abc@gmail.com',
-            },
-            locals: {
-                fname: 'ABC',
-                lname: 'ABC',
-            }
-        }).then(() => console.log('email has been sent!')); */
+        /*  email.send({
+             message: {
+                 from: 'abc@gmail.com',
+                 to: 'abc@gmail.com',
+             },
+             locals: {
+                 fname: 'ABC',
+                 lname: 'ABC',
+             }
+         }).then(() => console.log('email has been sent!')); */
     }).catch(console.error);
 
 
+}
+
+const sendEmail = () => {
+    let baseHTMLOptions = {
+        surveys: []
+    };
+
+    const baseHTML = pug.compileFile(__dirname + '/emails/table.pug');
+    let mailHTML = baseHTML(baseHTMLOptions);
+    console.log(mailHTML);
 }
 
 module.exports = sendEmail;
